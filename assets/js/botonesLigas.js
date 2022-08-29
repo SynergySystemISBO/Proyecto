9fd751292b31899cb22b96c206a71e788f739f10
Juan <juanigna818@gmail.com>
2022-08-29 20:32:11 -0300

    detalles y cambio de card

--- assets/js/botonesLigas.js
+++ assets/js/botonesLigas.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+    $('#tabla-fixture').show();
+    $('#estadisticas').hide();
+});
+
 $("#option1").change(function(){
     if(this.checked) {
         $('#tabla-fixture').show();
